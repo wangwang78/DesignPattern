94f35bc07c4122b515870694b1539d334ed68915
unlockzww <unlock_zww@163.com>
2025-07-20 21:44:58 +0800

    simple factory

---
+++ DesignPattern/patterns/SimpleFactory/IMessageSender.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPattern.patterns.SimpleFactory
+{
+    public interface IMessageSender
+    {
+        void Send(string message);
+    }
+
+    public class EmailSender : IMessageSender
+    {
+        public void Send(string message) => Console.WriteLine($"Email: {message}");
+    }
+
+    public class SmsSender : IMessageSender
+    {
+        public void Send(string message) => Console.WriteLine($"SMS: {message}");
+    }
+
+    public class MessageSenderFactory
+    {
+        public static IMessageSender Create(string type)
+        {
+            return type switch
+            {
+                "email" => new EmailSender(),
+                "sms" => new SmsSender(),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+
+}
